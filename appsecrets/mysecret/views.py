c81fd6e10e971be256d9527f2c411918c8e8c926
Sampul-CodeMine <studysampul@gmail.com>
2024-02-16 10:40:53 +0100

    Addition of the landing page for the appSecrets project

--- appsecrets/mysecret/views.py
+++ appsecrets/mysecret/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def landing(request):
+    context = {
+        'title': 'Home',
+    }
+    return render(request, 'mysecret/landing.html', context)
