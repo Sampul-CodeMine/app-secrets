5540bff1de5a7c5076f9e9f98de31a271ee2bbde
Sampul-CodeMine <studysampul@gmail.com>
2024-02-19 14:54:21 +0100

    Updated all the templates for the application and making sure all links are working fine.

--- appsecrets/mysecret/views.py
+++ appsecrets/mysecret/views.py
@@ -1,8 +1,70 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
-def landing(request):
+def landing_page(request):
+    """
+    This is the view to display the landing page
+    """
+    page = 'landing'
     context = {
         'title': 'Home',
     }
     return render(request, 'mysecret/landing.html', context)
+
+
+def profile_home(request):
+    """
+    This is the view to display the user's profile after successful login.
+    User must successfully login to access this page.
+    """
+    page = 'home'
+    context = {
+        'title': 'Profile'
+    }
+    return render(request, 'mysecret/profile.html', context)
+
+
+def about_page(request):
+    """
+    This is the view to display the about page. It does not require
+    login to access.
+    """
+    page = 'about'
+    context = {
+        'title': 'About'
+    }
+    return render(request, 'mysecret/about.html', context)
+
+
+def contact_page(request):
+    """
+    This is the view to display the Contact page. It does not require
+    login to access.
+    """
+    page = 'contact'
+    context = {
+        'title': 'Contact'
+    }
+    return render(request, 'mysecret/contact.html', context)
+
+
+def signin_user(request):
+    """
+    This is the view to display the Login page.
+    """
+    page = 'login'
+    context = {
+        'title': 'Sign In'
+    }
+    return render(request, 'mysecret/login.html', context)
+
+
+def signup_user(request):
+    """
+    This is the view to display the Registration page.
+    """
+    page = 'signup'
+    context = {
+        'title': 'Sign Up'
+    }
+    return render(request, 'mysecret/register.html', context)
